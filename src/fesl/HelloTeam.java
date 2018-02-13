761717a1194c41fa913998b59ba4fbbdc415bbab
afesl <afesl@student.tgm.ac.at>
2018-02-13 12:11:25 +0100

    erster commit

---
+++ src/fesl/HelloTeam.java
@@ -0,0 +1,15 @@
+package fesl;
+
+public class HelloTeam {
+
+	public static void halloFesl() {
+		System.out.println("Schönes Sommersemster wünscht Alexander Fesl");
+	}
+	
+	public static void main(String[] args) {
+		
+		halloFesl();  
+
+	}
+
+}
